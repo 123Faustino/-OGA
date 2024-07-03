2cdb7d8af111298d562040065e0d3c03bb33f6a6
cahoco <cahocofelizardo@gmail.com>
2024-07-03 16:46:04 +0100

    register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -30,15 +31,31 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:' . User::class,
+            'tel' => 'sometimes|string|max:255|unique:users,mobile',
+            'profile' => 'required|image|mimes:png,jpg,jpeg|max:1024',
+            'gender' => 'required',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
+        $profile = "";
+        if ($request->file("profile")) {
+            $request->file("profile")->store("profiles");
+            # code...
+        }
         $user = User::create([
-            'name' => $request->name,
+            'uuid' => Str::uuid(),
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'usernmae' => $request->usernmae,
+            'gender' => $request->gender ?? "",
+            'profile' => $profile ?? "",
             'email' => $request->email,
+            'mobile' => $request->tel ?? "",
             'password' => Hash::make($request->password),
+            'relationship' => "single"
         ]);
 
         event(new Registered($user));
