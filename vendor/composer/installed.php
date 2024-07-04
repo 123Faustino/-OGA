a04b3d0de30c4a0f278378bd57f2160d7196c5a3
cahoco <cahocofelizardo@gmail.com>
2024-07-04 16:44:00 +0100

    post

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'afb46e731f35078e5539bc891b095f6ba134e833',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'afb46e731f35078e5539bc891b095f6ba134e833',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pixelrobin/php-feather' => array(
+            'pretty_version' => 'v2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => '878334ee7512c9766d6a2f756734393aaf5f0044',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pixelrobin/php-feather',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
